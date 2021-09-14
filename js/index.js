d42e922a0fc5cf6893f30ce695196292b6bbd71d
Anatoliy Stepanchenko <stepanchenko1991@gmail.com>
2021-09-15 01:29:03 +0300

    added modal window

--- js/index.js
+++ js/index.js
@@ -92,4 +92,52 @@ function setClock(selector, endtime) {
 
  setClock('.timer', deadline);
 
+//  Modal
+
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+      modal = document.querySelector('.modal'),
+      modalCloseBtn = document.querySelector('[data-close]');
+
+function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+}
+
+modalTrigger.forEach(btn => {
+    btn.addEventListener('click', openModal);
+});
+
+function closeModal() {
+    modal.classList.add('hida');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+}
+
+modalCloseBtn.addEventListener('click', closeModal);
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        closeModal();
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+        closeModal();
+    }
+});
+
+const modalTimerId = setTimeout(openModal, 7000);
+
+function showModalByScrol () {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        openModal();
+        window.removeEventListener('scroll', showModalByScrol);
+    }
+}
+
+window.addEventListener('scroll', showModalByScrol);
+
 // });
