c53304eeb9c18e880033a0006c54cf33632327ca
Anatoliy Stepanchenko <stepanchenko1991@gmail.com>
2021-10-01 01:58:19 +0300

    f-n deleteNotDigits (regular expressions)

--- js/index.js
+++ js/index.js
@@ -380,10 +380,10 @@ for (let i = 0; i < slides.length; i++) {
 }
 
 next.addEventListener('click', () => {
-    if (offset == +width.slice(0, slides.length - 1) * (slides.length - 1)) {
+    if (offset == deleteNotDigits(width) * (slides.length - 1)) {
         offset = 0;
     } else {
-        offset += +width.slice(0, slides.length - 1);
+        offset += deleteNotDigits(width);
     } 
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -401,9 +401,9 @@ next.addEventListener('click', () => {
 
 prev.addEventListener('click', () => {
     if(offset == 0) {
-        offset = +width.slice(0, slides.length - 1) * (slides.length - 1);
+        offset = deleteNotDigits(width) * (slides.length - 1);
     } else {
-        offset -= +width.slice(0, slides.length - 1);
+        offset -= deleteNotDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -424,7 +424,7 @@ dots.forEach(dot => {
         const slideTo = e.target.getAttribute('data-slide-to');
 
         slideIndex = slideTo;
-        offset = +width.slice(0, slides.length - 1) * (slideTo - 1);
+        offset = deleteNotDigits(width) * (slideTo - 1);
 
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -446,6 +446,10 @@ function showCurrentSliderIndex() {
 function showCurrentDot() {
     dots.forEach(dot => dot.style.opacity = 0.5);
     dots[slideIndex - 1].style.opacity = 1;
+}
+
+function deleteNotDigits(str) {
+    return +str.replace(/\D/g,'');
 }
 
 // });
