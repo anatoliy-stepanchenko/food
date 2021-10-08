d868d99192c83960a4b76498c0edf0111467087f
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2021-10-08 17:39:30 +0300

    modules

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// window.addEventListener('DOMContentLoaded', () => {
+const calc = require('./modules/calc'),
+    cards = require('./modules/cards'),
+    forms = require('./modules/forms'),
+    modal = require('./modules/modal'),
+    slider = require('./modules/slider'),
+    tabs = require('./modules/tabs'),
+    tamer = require('./modules/tamer');
+
+    calc();
+    cards();
+    forms();
+    modal();
+    slider();
+    tabs();
+    tamer();
+// });
